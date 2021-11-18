70bf424b93265313fc977c1b1cb977e63c51fd1a
Asbjørn Hansen <ashan@cbb.dk>
2021-11-18 17:06:09 +0100

    set up the solution and class library with our existing sd file object (we need to go through this and make sure it's on par with the latest schema)

---
+++ Sre.ServiceDefinition/Options/Egress.cs
@@ -0,0 +1,13 @@
+namespace Sre.ServiceDefinition.Options
+{
+	public class Egress
+	{
+		public string Service { get; set; }
+
+		public string Namespace { get; set; }
+
+		public string Port { get; set; }
+
+		public string Protocol { get; set; }
+	}
+}
